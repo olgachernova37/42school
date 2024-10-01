89a9f30ee3701fbefa6a7fe357883edd8ec706a0
Olha Chernova <olcherno@c1r10s4.42prague.com>
2024-10-01 14:04:26 +0200

    sdfsdfcvd

--- 42_cursus/Libft/xxtests.c
+++ 42_cursus/Libft/xxtests.c
@@ -2,29 +2,16 @@
 // #include <ctype.h>
 #include <stdio.h>
 
-int	ft_isalpha(int c)
+int	ft_isdigit(int c);
+
+int main()
 {
-	int	result;
+    char c;
+    c='5';
+    printf("Result when numeric character is passed: %d", ft_isdigit(c));
 
-	result = 0;
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-		result = c;
-	}
-	return (result);
-}
-
-int	main(void)
-{
-	char c;
-	c = 'Q';
-	printf("\nResult when uppercase alphabet is passed: %d", ft_isalpha(c));
-
-	c = 'q';
-	printf("\nResult when lowercase alphabet is passed: %d", ft_isalpha(c));
-
-	c = '+';
-	printf("\nResult when non-alphabetic character is passed: %d", isalpha(c));
+    c='+';
+    printf("\nResult when non-numeric character is passed: %d", ft_isdigit(c));
 
 	return (0);
 }
