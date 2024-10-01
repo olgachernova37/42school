27c5004e59ad77bf8ceb236b2b6ddd2ec259acf3
Olha Chernova <olcherno@c1r10s4.42prague.com>
2024-10-01 14:59:12 +0200

    sdfsdfcvde

--- 42_cursus/Libft/ft_isprint.c
+++ 42_cursus/Libft/ft_isprint.c
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: olcherno <olcherno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 14:27:26 by olcherno          #+#    #+#             */
-/*   Updated: 2024/10/01 14:53:52 by olcherno         ###   ########.fr       */
+/*   Created: 2024/10/01 14:36:23 by olcherno          #+#    #+#             */
+/*   Updated: 2024/10/01 14:53:57 by olcherno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-
-# include <stddef.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-
-/* libc */
-int ft_isalpha(int c);
-int ft_isascii(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c <= 126);
+}
--- 42_cursus/Libft/ft_strlen.c
+++ 42_cursus/Libft/ft_strlen.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: olcherno <olcherno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 14:27:26 by olcherno          #+#    #+#             */
-/*   Updated: 2024/10/01 14:53:52 by olcherno         ###   ########.fr       */
+/*   Created: 2024/10/01 14:37:56 by olcherno          #+#    #+#             */
+/*   Updated: 2024/10/01 14:56:41 by olcherno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
+size_t	ft_strlen(const char *str)
+{
+	int	i;
 
-/* libc */
-int ft_isalpha(int c);
-int ft_isascii(int c);
-
-#endif
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
