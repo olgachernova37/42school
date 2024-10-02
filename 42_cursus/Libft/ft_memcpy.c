f88251113ea4355766dc0a2943ae2d412fe1b9d1
Olha Chernova <olcherno@c1r10s4.42prague.com>
2024-10-02 17:56:15 +0200

    dsdd

--- 42_cursus/Libft/ft_memcpy.c
+++ 42_cursus/Libft/ft_memcpy.c
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: olcherno <olcherno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 15:54:16 by olcherno          #+#    #+#             */
-/*   Updated: 2024/09/23 15:55:49 by olcherno         ###   ########.fr       */
+/*   Created: 2024/10/02 17:49:27 by olcherno          #+#    #+#             */
+/*   Updated: 2024/10/02 17:52:05 by olcherno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int c)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int	result;
+	unsigned char		*u_dest;
+	const unsigned char	*u_src;
 
-	result = 0;
-	if ((c >= 48 && c <= 57))
-	{
-		result = c;
-	}
-	return (result);
+	if (n == 0)
+		return (dest);
+	if (!dest || !src)
+		return (NULL);
+	u_dest = (unsigned char *)dest;
+	u_src = (const unsigned char *)src;
+	while (n--)
+		*u_dest++ = *u_src++;
+	return (dest);
 }
--- 42_cursus/Libft/ft_memset.c
+++ 42_cursus/Libft/ft_memset.c
@@ -6,10 +6,23 @@
 /*   By: olcherno <olcherno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:25:28 by olcherno          #+#    #+#             */
-/*   Updated: 2024/10/02 14:25:28 by olcherno         ###   ########.fr       */
+/*   Updated: 2024/10/02 17:49:08 by olcherno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "libft.h"
+#include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char temp_c;
+	unsigned char *temp_s;
+
+	if (!s)
+		return (NULL);
+	temp_c = (unsigned char)c;
+	temp_s = (unsigned char *)s;
+	while (n--)
+		*temp_s++ = temp_c;
+
+	return (s);
+}
